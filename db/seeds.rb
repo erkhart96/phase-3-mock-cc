7f2b6a8e1e9fcda564012ee92dbb1026b4d5ff03
Ben <berkhart@rocketmail.com>
2022-09-28 10:33:18 -0500

    Code challenge practice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,22 @@ Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+Dev.create(name: "Rick", company_id: 1)
+Dev.create(name: "Morty", company_id: 2)
+Dev.create(name: "Mr. Meseeks", company_id: 3)
+Dev.create(name: "Gazorpazop", company_id: 4)
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Water Bottle", value: 1, freebie_id: 1)
+Freebie.create(item_name: "Stickers", value: 5, freebie_id: 2)
+Freebie.create(item_name: "Cup", value: 1, freebie_id: 3)
+Freebie.create(item_name: "Shirt", value: 1, freebie_id: 4)
+Freebie.create(item_name: "Hoodie", value: 1, freebie_id: 5)
+
+
+
+
+
 
 puts "Seeding done!"
